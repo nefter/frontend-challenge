7d772453532e69548850a623907afbf4374ebe72
Nef Rossano <nefter@users.noreply.github.com>
2021-03-04 21:53:39 -0600

    Se actualiza proyecto

---
+++ src/env.js
@@ -0,0 +1,4 @@
+export const apitoken = 'CFPAT-LBtveUvtDi7YjAhsyNzZURthngcrVnIr53eOZjYnxuc';
+export const apispace = '1t4hjzo7y0kb';
+export const apienv = 'master';
+export const apicontenttype='audiocontent-v6';
